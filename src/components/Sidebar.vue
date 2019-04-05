108546846844e7484c8649153cea63bf9383f0ec
Kathleen Xu <klx2@cornell.edu>
2019-04-05 15:09:20 -0400

    styled sidebar

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -1,7 +1,24 @@
 <template>
-    <b-nav>
-        <b-nav-item to="/">Home</b-nav-item>
-        <b-nav-item to="/about">About</b-nav-item>
-        <b-nav-item to="/profile">Edit Profile</b-nav-item>
+    <b-nav class="flexible-column">
+        <div class="nav-item">
+            <b-nav-item to="/">Home</b-nav-item>
+            <b-nav-item to="/about">About</b-nav-item>
+            <b-nav-item to="/profile">Edit Profile</b-nav-item>
+        </div>
     </b-nav>
 </template>
+
+<script>
+
+</script>
+<style lang="scss" scoped>
+    .nav-item{
+        text-align:left;
+        background-color:#373737;
+        list-style:none;
+        a{
+            color:white;
+            text-decoration:none;
+        }
+    }
+</style>
