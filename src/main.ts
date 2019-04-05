1c7f7bfad325f2fa648e86e03a9548beb72a2d75
Kathleen Xu <klx2@cornell.edu>
2019-04-05 11:52:54 -0400

    installed bootstrap-vue

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,9 @@
 import Vue from "vue";
+import BootstrapVue from "bootstrap-vue";
 import App from "./App.vue";
 import router from "./router";
+
+Vue.use(BootstrapVue);
 
 Vue.config.productionTip = false;
 
