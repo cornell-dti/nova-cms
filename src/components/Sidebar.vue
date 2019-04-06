ca8d82e59ad2b249ad150837a550ef3c4c2505bc
Kathleen Xu <klx2@cornell.edu>
2019-04-06 14:27:46 -0400

    imported bootstrap i guess

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -1,5 +1,5 @@
 <template>
-    <b-nav class="flexible-column">
+    <b-nav class="flex-column">
         <div class="nav-item">
             <b-nav-item to="/">Home</b-nav-item>
             <b-nav-item to="/about">About</b-nav-item>
@@ -14,10 +14,9 @@
 <style lang="scss" scoped>
     .nav-item{
         text-align:left;
-        background-color:#373737;
         list-style:none;
         a{
-            color:white;
+            color:black;
             text-decoration:none;
             &.router-link-exact-active {
             font-weight:bold;
