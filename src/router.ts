77f230a4b93315038765361301548ebb6bdc5671
Kathleen Xu <klx2@cornell.edu>
2019-04-21 19:47:52 -0400

    router edit

--- src/router.ts
+++ src/router.ts
@@ -27,6 +27,12 @@ export default new Router({
       // which is lazy-loaded when the route is visited.
       component: () =>
         import(/* webpackChunkName: "about" */ "./views/About.vue")
+    },
+    {
+      path:"/admin",
+      name:"admin", 
+      component: () =>
+        import("./views/Admin.vue")
     }
   ]
 });
